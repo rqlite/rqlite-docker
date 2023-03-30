d9154686e72bdf8dc5b391b5bcb8434a4aeb2424
Philip O'Toole <philip.otoole@yahoo.com>
2023-03-30 00:05:13 -0400

    GPT-improved Docker entry-point script

--- 7.14.1/docker-entrypoint.sh
+++ 7.14.1/docker-entrypoint.sh
@@ -1,62 +1,45 @@
 #!/bin/sh
 
 contains() {
-	key=$1
-	shift
-	for i in "$@"
-	do
-		if [[ $i == $key* ]]; then
-			return 1
-		fi
-	done
-	return 0
+    local key=$1
+    shift
+    for i in "$@"
+    do
+        if [[ $i == "$key"* ]]; then
+            return 1
+        fi
+    done
+    return 0
 }
 
-DEFAULT_NODE_ID=`hostname`
-DEFAULT_ADV_ADDRESS=`hostname -f`
+set_option() {
+    local contains_key=$1
+    local env_var_name=$2
+    local default_value=$3
+    local option_name=$4
 
-contains "-http-addr" "$@"
-if [ $? -eq 0 ]; then
-	if [ -z "$HTTP_ADDR" ]; then
-		HTTP_ADDR="0.0.0.0:4001"
-	fi
-	http_addr="-http-addr $HTTP_ADDR"
-fi
+    contains "$contains_key" "$@"
+    if [ $? -eq 0 ]; then
+        if [ -z "${!env_var_name}" ]; then
+            eval "$env_var_name=\"$default_value\""
+        fi
+        echo "$option_name ${!env_var_name}"
+    else
+        echo ""
+    fi
+}
 
-contains "-http-adv-addr" "$@"
-if [ $? -eq 0 ]; then
-	if [ -z "$HTTP_ADV_ADDR" ]; then
-		HTTP_ADV_ADDR=$DEFAULT_ADV_ADDRESS:4001
-	fi
-	http_adv_addr="-http-adv-addr $HTTP_ADV_ADDR"
-fi
+DEFAULT_NODE_ID=$(hostname)
+DEFAULT_ADV_ADDRESS=$(hostname -f)
 
-contains "-raft-adv" "$@"
-if [ $? -eq 0 ]; then
-	if [ -z "$RAFT_ADDR" ]; then
-		RAFT_ADDR="0.0.0.0:4002"
-	fi
-	raft_addr="-raft-addr $RAFT_ADDR"
-fi
-
-contains "-raft-adv-addr" "$@"
-if [ $? -eq 0 ]; then
-	if [ -z "$RAFT_ADV_ADDR" ]; then
-		RAFT_ADV_ADDR=$DEFAULT_ADV_ADDRESS:4002
-	fi
-	raft_adv_addr="-raft-adv-addr $RAFT_ADV_ADDR"
-fi
-
-contains "-node-id" "$@"
-if [ $? -eq 0 ]; then
-	if [ -z "$NODE_ID" ]; then
-		NODE_ID="$DEFAULT_NODE_ID"
-	fi
-	node_id="-node-id $NODE_ID"
-fi
+http_addr=$(set_option "-http-addr" "HTTP_ADDR" "0.0.0.0:4001" "-http-addr")
+http_adv_addr=$(set_option "-http-adv-addr" "HTTP_ADV_ADDR" "$DEFAULT_ADV_ADDRESS:4001" "-http-adv-addr")
+raft_addr=$(set_option "-raft-addr" "RAFT_ADDR" "0.0.0.0:4002" "-raft-addr")
+raft_adv_addr=$(set_option "-raft-adv-addr" "RAFT_ADV_ADDR" "$DEFAULT_ADV_ADDRESS:4002" "-raft-adv-addr")
+node_id=$(set_option "-node-id" "NODE_ID" "$DEFAULT_NODE_ID" "-node-id")
 
 if [ -z "$DATA_DIR" ]; then
-	DATA_DIR="/rqlite/file/data"
+    DATA_DIR="/rqlite/file/data"
 fi
 
 # When running on Kubernetes, delay a small time so DNS records
@@ -69,13 +52,12 @@ fi
 #
 # This is kind of a hack. If anyone knows a better way file
 # a GitHub issue at https://github.com/rqlite/rqlite-docker.
-if [ -n "$KUBERNETES_SERVICE_HOST" ]; then
-      if [ -z "$START_DELAY" ]; then
-            START_DELAY=5
-      fi
+if [ -n "$KUBERNETES_SERVICE_HOST" ] && [ -z "$START_DELAY" ]; then
+    START_DELAY=5
 fi
+
 if [ -n "$START_DELAY" ]; then
-      sleep "$START_DELAY"
+    sleep "$START_DELAY"
 fi
 
 RQLITED=/bin/rqlited
@@ -83,10 +65,9 @@ rqlited_commands="$RQLITED $node_id $http_addr $http_adv_addr $raft_addr $raft_a
 data_dir="$DATA_DIR"
 
 if [ "$1" = "rqlite" ]; then
-        set -- $rqlited_commands $data_dir
+    set -- $rqlited_commands $data_dir
 elif [ "${1:0:1}" = '-' ]; then
-        # User is passing some options, so merge them.
-        set -- $rqlited_commands $@ $data_dir
+    set -- $rqlited_commands "$@" $data_dir
 fi
 
 exec "$@"
